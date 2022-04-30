6866811632f14d0a76c3c822fbec38568b5d4def
Wonsang <temporaryaion1@hotmail.com>
2022-05-01 00:10:49 +0900

    feat: add code to created() hook

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ export function createApp() {
   return createSSRApp({
     data: () => ({ count: 1, intervalTimer: null }),
     template: `<div @click="handleClick()">{{ count }}</div>`,
-    // template: `<p><div @click="handleClick()">{{ count }}</div></p>`,
-    // created () {
-    //   this.intervalTimer = setInterval(() => {
-    //     this.count++;
-    //     console.log('increment from setInterval; count:', this.count);
+    // beforeCreate () {
+    //   setInterval(() => {
+    //     console.log('called from beforeCreate');
     //   }, 1000)
     // },
+    created () {
+      this.intervalTimer = setInterval(() => {
+        this.count++;
+        console.log('increment from setInterval; count:', this.count);
+      }, 1000)
+    },
     methods: {
       handleClick () {
         this.count++;
         console.log('BUTTON CLICKED!');
       }
     },
-    // beforeUnmount () {
-    //   clearInterval(this.intervalTimer)
-    // }
+    beforeUnmount () {
+      clearInterval(this.intervalTimer)
+    }
   });
 }
