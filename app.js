66f54221fcc842a30bba44603bb08384e9060e23
Wonsang <sang.jeong@freedgrouptech.com>
2022-04-30 23:32:10 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import { createSSRApp } from 'vue';
+
+export function createApp() {
+  return createSSRApp({
+    data: () => ({ count: 1, intervalTimer: null }),
+    template: `<p><div @click="handleClick()">{{ count }}</div></p>`,
+    // created () {
+    //   this.intervalTimer = setInterval(() => {
+    //     this.count++;
+    //     console.log('increment from setInterval; count:', this.count);
+    //   }, 1000)
+    // },
+    methods: {
+      handleClick () {
+        this.count++;
+        console.log('BUTTON CLICKED!');
+      }
+    },
+    // beforeUnmount () {
+    //   clearInterval(this.intervalTimer)
+    // }
+  });
+}
